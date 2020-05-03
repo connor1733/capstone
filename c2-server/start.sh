8bb1a4b3d1d9b55d91cbcbefbeef81571b546f7c
Patrick Casey <patcasey@umass.edu>
2020-05-03 17:06:28 -0400

    changed sleep to 8

--- c2-server/start.sh
+++ c2-server/start.sh
@@ -2,8 +2,8 @@
 
 python3 implant-server.py & 
 echo "Implant server started on port 8080"
-sleep 5
+sleep 8
 sudo python3 -m http.server 80 &
 echo "Started HTTP server on port 80"
-sleep 5
+sleep 8
 sudo python3 server.py
