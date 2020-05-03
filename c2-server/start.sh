456a5c55a8386387eced721cb5a6da46b488e2fa
Patrick Casey <patcasey@umass.edu>
2020-05-03 16:53:44 -0400

    Reduced sleep time to 5 seconds

--- c2-server/start.sh
+++ c2-server/start.sh
@@ -2,8 +2,8 @@
 
 python3 implant-server.py & 
 echo "Implant server started on port 8080"
-sleep 10
+sleep 5
 sudo python3 -m http.server 80 &
 echo "Started HTTP server on port 80"
-sleep 10
+sleep 5
 sudo python3 serverpy
